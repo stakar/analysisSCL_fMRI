3af652dad715aa511ef568bc95088f98d96f4906
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-04-12 20:03:37 +0200

    Fixed error with filtering

--- analysis.py
+++ analysis.py
@@ -1,8 +1,11 @@
-import numpy as np
+import numpy as np 
 import pandas as pd
 from analysis_tools import *
 from os import listdir
+import time
 
+ORIGINF=5000
+FREQ=100
 #check the time, for counting time necessary for analysis.
 curr = time.time()
 
@@ -14,9 +17,16 @@ filelist = [n.split('.')[0] for n in listdir('analiza_treatment/') \
 for file in filelist:
     try:        
         if f"{file}.xlsx" not in listdir('preprocessed_treatment/'):
-            analysis(f'analiza_treatment/{file}.vhdr',
-                     f'analiza_treatment/{file}.vmrk',
-                     f'preprocessed_treatment/{file}')
+            
+            signal = get_bv_medfiltered_signal(f'analiza_treatment/{file}.vhdr',
+                                           f'analiza_treatment/{file}.vmrk',
+                                           f'preprocessed_treatment/{file}')
+            calculate_save_results(signal[0],
+                       signal[2],
+                       signal[1],
+                       file,
+                       f'preprocessed_treatment/{file}',
+                       output_aggr = 'output_TP1')
     except Exception as e:
         print(e)
         print(f'Problem with {file}')
@@ -29,24 +39,37 @@ filelist = [n.split('.')[0] for n in listdir('analiza_treatment/') if \
 for file in filelist:
     try:
         if f"{file}.xlsx" not in listdir('preprocessed_treatment/'):
-            analysis(f'analiza_treatment/{file}.vhdr',
-                     f'analiza_treatment/{file}.vmrk',
-                     f'preprocessed_treatment/{file}')
+            signal = get_bv_medfiltered_signal(f'analiza_treatment/{file}.vhdr',
+                                           f'analiza_treatment/{file}.vmrk',
+                                           f'preprocessed_treatment/{file}')
+            calculate_save_results(signal[0],
+                       signal[2],
+                       signal[1],
+                       file,
+                       f'preprocessed_treatment/{file}',
+                       output_aggr = 'output_TP5')
     except Exception as e:
         print(e)
         print(f'Problem with {file}')
         
         
 #Get list of files from time point 6
-filelist = [n.split('.')[0] for n in listdir('analiza_follow up/') if \
-            ('acq' in n and 'tp6' in n.lower())]
-#preprocess each file from time point 6, if not present.
+filelist = [n.split('.')[0] for n in listdir('analiza_follow up/') if ('acq' in n and 'tp6' in n.lower())]
+
 for file in filelist:
     try:
         if f"{file}.xlsx" not in listdir('preprocessed_fup/'):
-            analysis_acknowledge(f'analiza_follow up/{file}.acq',
-                             f'analiza_follow up/{file}.acq',
-                             f'preprocessed_fup/{file}')
+            signal = get_acq_medfiltered_signal(f'analiza_follow up/{file}.acq',
+                                           f'analiza_follow up/{file}.acq',
+                                           f'preprocessed_fup/{file}',
+                                          freq=100,
+                                           originf=2000)
+            calculate_save_results(signal[0],
+                               signal[2],
+                               signal[1],
+                               file,
+                               f'preprocessed_fup/{file}',
+                               output_aggr = 'output_TP6')
     except Exception as e:
         print(e)
         print(f'Problem with {file}')
@@ -58,12 +81,21 @@ filelist = [n.split('.')[0] for n in listdir('analiza_follow up/') \
 for file in filelist:
     try:
         if f"{file}.xlsx" not in listdir('preprocessed_fup_TP7/'):
-            analysis_acknowledge(f'analiza_follow up/{file}.acq',
-                                 f'analiza_follow up/{file}.acq',
-                                 f'preprocessed_fup_TP7/{file}')
+            signal = get_acq_medfiltered_signal(f'analiza_follow up/{file}.acq',
+                                           f'analiza_follow up/{file}.acq',
+                                           f'preprocessed_fup_TP7/{file}',
+                                          freq=100,
+                                           originf=2000)
+            calculate_save_results(signal[0],
+                               signal[2],
+                               signal[1],
+                               file,
+                               f'preprocessed_fup_TP7/{file}',
+                               output_aggr = 'output_TP7')
     except Exception as e:
         print(e)
         print(f'Problem with {file}')
+        
         
         
 #check post-time, count difference and show how much time was spent.
