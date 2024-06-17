f3c4ea5f67ab9227231a2f688b731f49c3d341b9
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-06-17 11:11:04 +0200

    Mjr updates, normalized signal change with bsl
    Major updates, changed to calculating normalizaed change of signal based on universal baseline.

--- analysis_tools.py
+++ analysis_tools.py
@@ -121,6 +121,55 @@ def convert_acq_mne(filename='analiza_follow up\JM22a_B140_TP6_evt.acq'):
     raw = mne.io.RawArray(data, info)
     return raw
 
+### I comment this function, lots of blunders (medfilter must be changed, data is always converting same file etc.
+# def analysis_acknowledge(filename_signal, filename_events, output, freq=FREQ, originf=ORIGINF, cutoff=2, correction=1):
+#     """
+#     Conduct analysis- open file, preprocess data (resample, filter with lowpass filter, 
+#     conduct median smoothing, get stimuli time points and calculate results.
+#     """
+#     print(f'opening {filename_signal}...')
+#     data = convert_acq_mne('analiza_follow up\JM22A_B052_TP6.acq')
+#     resampled = data.copy().resample(sfreq=freq).get_data()[0]
+#     filtered = mne.filter.filter_data(resampled,FREQ,None,cutoff)
+#     medfiltered=medfilt(filtered,int((FREQ/2)-1))
+#     stimlist = create_stimlist_file_from_data(filename_events)
+#     stimarr = get_stimulus_array(stimlist)
+#     stimnames = get_stimulus_names(stimlist)
+#     new_stims= resample_events(stimarr,originf/freq)
+#     print('calculating results...')
+#     result = []
+#     for name,datapoint in zip(stimnames,new_stims):
+#         tmp_bsle=np.mean(filtered[datapoint-(3*FREQ):datapoint])*correction
+#         tmp_mean30=np.mean(filtered[datapoint:datapoint+(30*FREQ)])*correction
+#         tmp_mean15=np.mean(filtered[datapoint:datapoint+(15*FREQ)])*correction
+#         tmp_mean12=np.mean(filtered[datapoint:datapoint+(12*FREQ)])*correction
+
+#         tmp_bsle=np.min(filtered[datapoint-(3*FREQ):datapoint])*correction
+#         tmp_min30=np.min(filtered[datapoint:datapoint+(30*FREQ)])*correction
+#         tmp_min15=np.min(filtered[datapoint:datapoint+(15*FREQ)])*correction
+#         tmp_min12=np.min(filtered[datapoint:datapoint+(12*FREQ)])*correction
+
+#         tmp_bsle=np.max(filtered[datapoint-(3*FREQ):datapoint])*correction
+#         tmp_max30=np.max(filtered[datapoint:datapoint+(30*FREQ)])*correction
+#         tmp_max15=np.max(filtered[datapoint:datapoint+(15*FREQ)])*correction
+#         tmp_max12=np.max(filtered[datapoint:datapoint+(12*FREQ)])*correction
+
+#         for suffix,suf_value in zip(['_baseline','_mean30','_mean15','_mean12','_min30',
+#                                      '_min15','_min12','_max30','_max15','_max12'],
+#                                     [tmp_bsle,tmp_mean30,tmp_mean15,tmp_mean12,tmp_min30,
+#                                      tmp_min15,tmp_min12,tmp_max30,tmp_max15,tmp_max12]):
+#             value_name = f'{name}{suffix}'
+#             try:
+#                 if value_name in np.array(result)[:,0]:
+#                     result.append((f'{value_name}_2',suf_value))
+#                 else:
+#                     result.append((value_name,suf_value))
+#             except:
+#                 result.append((value_name,suf_value))
+#     result = DataFrame(result)
+#     result.to_excel(f'{output}.xlsx')
+#     return result
+
 def create_placeholder(reference='preprocessed_fup\JM22A_B011_TP6.xlsx', output='result.xlsx'):
     if output not in listdir():
         tmp = read_excel(filename)
@@ -184,9 +233,13 @@ def calc_normalized_signal_change(signal,preceding_signal):
 def get_normalized_signal_change(signal,
                                  point,
                                  time_window=27*100,
-                                 time_window_preceding=3*100):
+                                 time_window_preceding=3*100,
+                                baseline = None):
     partSig = signal[point:(point+time_window)]
-    precedSig = signal[point-time_window_preceding:point]
+    if baseline==None:
+        precedSig = signal[point-time_window_preceding:point]
+    else:
+        precedSig = baseline
     return calc_normalized_signal_change(partSig,precedSig)
 
 def get_acq_medfiltered_signal(filename_signal, 
@@ -236,78 +289,15 @@ def plot_acq_medfiltered_signal(filename_signal,
     stimnames = get_stimulus_names(stimlist)
     new_stims= resample_events(stimarr,originf/freq)
     plt.plot([n for n in range(0,len(resampled),1)],resampled)
+    
     plt.plot([n for n in range(0,len(resampled),1)],filtered)
+    
     plt.plot([n for n in range(0,len(resampled),1)],medfiltered)
     for stim in new_stims:
         plt.axvspan(stim-100, stim+100, facecolor='r',alpha=0.5)
+    plt.xlim(100000,110000)
     return medfiltered,new_stims,stimnames
 
-def plot_bv_medfiltered_signal(filename_signal,
-                              filename_events,
-                              output,
-                              freq=FREQ,
-                              originf=ORIGINF,
-                              cutoff=2,
-                              correction=1,
-                             time_window_analyzed=27):
-    """
-    Conduct analysis- open file, preprocess data (resample, filter with lowpass filter, 
-    conduct median smoothing, get stimuli time points and calculate results.
-    """
-    print(f'opening {filename_signal}...')
-    data = mne.io.read_raw_brainvision(filename_signal)
-    resampled = data.copy().resample(sfreq=freq).get_data().flatten()
-    filtered = mne.filter.filter_data(resampled,freq,None,cutoff)
-    medfiltered=medfilt(filtered,int((freq/2)-1))
-    stimlist = get_stimlist(filename_events)
-    stimarr = get_stimulus_array(stimlist)
-    stimnames = get_stimulus_names(stimlist)
-    new_stims= resample_events(stimarr,originf/freq)
-    plt.plot([n for n in range(0,len(resampled),1)],resampled)
-    plt.plot([n for n in range(0,len(resampled),1)],filtered)
-    plt.plot([n for n in range(0,len(resampled),1)],medfiltered)
-    for stim in new_stims:
-        plt.axvspan(stim-100, stim+100, facecolor='r',alpha=0.5)
-    return medfiltered,new_stims,stimnames
-
-def get_bv_medfiltered_signal(filename_signal,
-                              filename_events,
-                              output,
-                              freq=FREQ,
-                              originf=ORIGINF,
-                              cutoff=2,
-                              correction=1,
-                             time_window_analyzed=27):
-    """
-    Conduct analysis- open file, preprocess data (resample, filter with lowpass filter, 
-    conduct median smoothing, get stimuli time points and calculate results.
-    """
-    print(f'opening {filename_signal}...')
-    data = mne.io.read_raw_brainvision(filename_signal)
-    resampled = data.copy().resample(sfreq=freq).get_data().flatten()
-    filtered = mne.filter.filter_data(resampled,freq,None,cutoff)
-    medfiltered=medfilt(filtered,int((freq/2)-1))
-    stimlist = get_stimlist(filename_events)
-    stimarr = get_stimulus_array(stimlist)
-    stimnames = get_stimulus_names(stimlist)
-    new_stims= resample_events(stimarr,originf/freq)
-    return medfiltered,new_stims,stimnames
-
-def calculate_results(signal,triggers,freq,time_window_analyzed):
-    plc = np.zeros([len(triggers),freq*time_window_analyzed])
-    for n in range(len(triggers)):
-        plc[n] = signal[triggers[n]:(triggers[n]+freq*time_window_analyzed)]
-    avg = np.mean(plc,axis=0)
-    return ({'mean_slope' : np.mean([get_slope(plc[n]) for n in range(len(plc))]),
-            'mean_normalized_signal_change' : np.mean([get_normalized_signal_change(signal,n)\
-                                                                 for n in triggers]),
-            'mean_sig_sign_alteration' : np.mean([get_ssa(plc[n]) for n in range(len(plc))]),
-            'mean_slope_2pt' : np.mean([get_slope_2_points(plc[n]) for n in range(len(plc))]),
-           'mean_signal' : np.mean([np.mean(plc[n]) for n in range(len(plc))]),
-           'mean_max_signal' : np.mean([np.max(plc[n]) for n in range(len(plc))]),
-           'mean_min_signal' : np.mean([np.min(plc[n]) for n in range(len(plc))])},
-           avg)
-            
 def calculate_save_results(signal,
                            events,
                            events_times,
@@ -336,6 +326,157 @@ def calculate_save_results(signal,
             triggers = events_times[triggers][0::2]
             
             result,avg = calculate_results(signal,triggers,freq,time_window_analyzed)
+            result = DataFrame(result, 
+                               index = pd.Index([re.search(r'B\d+',filename_signal)[0]]))
+            avg = pd.DataFrame(avg,columns=['averaged_signal'])
+            try:
+                with pd.ExcelWriter(f'{output}.xlsx',
+                                mode='a',
+                                if_sheet_exists='overlay') as writer:  
+                    result.to_excel(writer, 
+                                sheet_name=f'{condition}_{part}')  
+                    avg.to_excel(writer, 
+                             sheet_name=f'{condition}_{part}_avg')
+            except FileNotFoundError:
+                result.to_excel(f'{output}.xlsx', 
+                                sheet_name=f'{condition}_{part}')  
+                with pd.ExcelWriter(f'{output}.xlsx',
+                                mode='a',
+                                if_sheet_exists='overlay') as writer: 
+                    avg.to_excel(writer, 
+                             sheet_name=f'{condition}_{part}_avg')
+            try:
+                with pd.ExcelWriter(f'{output_aggr}.xlsx',
+                                    mode='a',
+                                    if_sheet_exists='overlay') as writer:  
+                    avg.to_excel(writer, 
+                                 sheet_name=f'{condition}_{part}_avg',
+                                 header=False)
+                    result.to_excel(writer, 
+                                    sheet_name=f'{condition}_{part}',
+                                    startrow=writer.sheets[f'{condition}_{part}'].max_row,
+                                    header=False)
+            except FileNotFoundError:
+                result.to_excel(f'{output_aggr}.xlsx', 
+                                sheet_name=f'{condition}_{part}')
+                with pd.ExcelWriter(f'{output_aggr}.xlsx',
+                                    mode='a',
+                                    if_sheet_exists='overlay') as writer:  
+                    avg.to_excel(writer, 
+                                 sheet_name=f'{condition}_{part}_avg',
+                                 header=False)
+            except KeyError:
+                with pd.ExcelWriter(f'{output_aggr}.xlsx',
+                                                mode='a',
+                                                if_sheet_exists='overlay') as writer:  
+                    avg.to_excel(writer, 
+                                 sheet_name=f'{condition}_{part}_avg',
+                                 header=False)
+                    result.to_excel(writer, 
+                                    sheet_name=f'{condition}_{part}',
+                                    header=False)
+                    
+def get_bv_medfiltered_signal(filename_signal,
+                              filename_events,
+                              output,
+                              freq=FREQ,
+                              originf=ORIGINF,
+                              cutoff=2,
+                              correction=1,
+                             time_window_analyzed=27):
+    """
+    Conduct analysis- open file, preprocess data (resample, filter with lowpass filter, 
+    conduct median smoothing, get stimuli time points and calculate results.
+    """
+    print(f'opening {filename_signal}...')
+    data = mne.io.read_raw_brainvision(filename_signal)
+    resampled = data.copy().resample(sfreq=freq).get_data().flatten()
+    filtered = mne.filter.filter_data(resampled,freq,None,cutoff)
+    medfiltered=medfilt(filtered,int((freq/2)-1))
+    stimlist = get_stimlist(filename_events)
+    stimarr = get_stimulus_array(stimlist)
+    stimnames = get_stimulus_names(stimlist)
+    new_stims= resample_events(stimarr,originf/freq)
+    return medfiltered,new_stims,stimnames
+
+def get_bv_zscore_signal(filename_signal,
+                              filename_events,
+                              output,
+                              freq=FREQ,
+                              originf=ORIGINF,
+                              cutoff=2,
+                              correction=1,
+                             time_window_analyzed=27,
+                               medfilt_kernel = int((FREQ/2)-1)):
+    """
+    Conduct analysis- open file, preprocess data (resample, filter with lowpass filter, 
+    conduct median smoothing, get stimuli time points and calculate results.
+    """
+    print(f'opening {filename_signal}...')
+    data = mne.io.read_raw_brainvision(filename_signal)
+    resampled = data.copy().resample(sfreq=freq).get_data().flatten()
+    filtered = mne.filter.filter_data(resampled,freq,None,cutoff)
+    medfiltered=medfilt(filtered,medfilt_kernel)
+    stimlist = get_stimlist(filename_events)
+    stimarr = get_stimulus_array(stimlist)
+    stimnames = get_stimulus_names(stimlist)
+    new_stims= resample_events(stimarr,originf/freq)
+    zscored = stats.zscore(medfiltered)
+    return medfiltered,new_stims,stimnames,zscored
+
+
+def calculate_results(signal,triggers,freq,time_window_analyzed,bsl_ind=None):
+    plc = np.zeros([len(triggers),freq*time_window_analyzed])
+    for n in range(len(triggers)):
+        plc[n] = signal[triggers[n]:(triggers[n]+freq*time_window_analyzed)]
+    avg = np.mean(plc,axis=0)
+    if bsl_ind==None:
+        mnsc =  np.mean([get_normalized_signal_change(signal,n) for n in triggers])
+    else:
+        baseline = signal[bsl_ind-3*freq:bsl_ind]
+        mnsc = np.mean([get_normalized_signal_change(signal,n) for n in triggers])
+    return ({'mean_slope' : np.mean([get_slope(plc[n]) for n in range(len(plc))]),
+            'mean_normalized_signal_change' : mnsc,
+            'mean_sig_sign_alteration' : np.mean([get_ssa(plc[n]) for n in range(len(plc))]),
+            'mean_slope_2pt' : np.mean([get_slope_2_points(plc[n]) for n in range(len(plc))]),
+           'mean_signal' : np.mean([np.mean(plc[n]) for n in range(len(plc))]),
+           'mean_max_signal' : np.mean([np.max(plc[n]) for n in range(len(plc))]),
+           'mean_min_signal' : np.mean([np.min(plc[n]) for n in range(len(plc))])},
+           avg)
+            
+
+                    
+def calculate_save_results_bsl(signal,
+                           events,
+                           events_times,
+                           filename_signal,
+                           output,
+                           freq=FREQ,
+                           time_window_analyzed=27,
+                           output_aggr = 'output2',
+                          parts=['1','2','3','4','123'],
+                          conditions=['crit','neut'],
+                          regexp_set=f'S 11[]$'):
+    """
+    Conduct analysis- open file, preprocess data (resample, filter with lowpass filter, 
+    conduct median smoothing, get stimuli time points and calculate results.
+    """
+    print('calculating results...')
+    for part in parts:
+        for condition in conditions:
+            if condition == 'crit':
+                regexp = fr'S [12345][{part}]$'
+            elif condition=='neut':
+                regexp = fr'S [6789][{part}]$|S 10[{part}]$'
+            else:
+                regexp = regexp_set
+            triggers = list(map(lambda x: bool(re.match(regexp, x)), events))
+            triggers = events_times[triggers][0::2]
+            bsl_name = events[list(map(lambda x: bool(re.match("S [1-9][1-9]$", x)), events))][0]
+            bsl_index = events_times[list(map(lambda x: bool(re.match("S [1-9]", x)), events))][0]
+            print(f'Baseline event name is: {bsl_name}, index is {bsl_index}')
+            
+            result,avg = calculate_results(signal,triggers,freq,time_window_analyzed,bsl_ind=bsl_index)
             result = DataFrame(result, 
                                index = pd.Index([re.search(r'B\d+',filename_signal)[0]]))
             avg = pd.DataFrame(avg,columns=['averaged_signal'])
